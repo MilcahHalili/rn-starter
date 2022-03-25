9e88b70ff20a22cf45a1cda158d511d08eeb6033
Milcah <code@milcah.dev>
2022-03-24 21:49:58 -0700

    creates flatlist

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,12 +1,33 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { FlatList, View, Text, StyleSheet } from 'react-native'
 
 const ListScreen = () => {
+  const friends = [
+    {name: 'Ramisha', age: '22'},
+    {name: 'Fatima', age: '28'},
+    {name: 'May', age: '43'},
+    {name: 'Ashleigh', age: '32'},
+    {name: 'Reyna', age: '36'},
+    {name: 'Rhea', age: '40'},
+    {name: 'Olive', age: '11'},
+    {name: 'Jasmine', age: '24'},
+    {name: 'Cee', age: '42'}
+  ]
   return (
-    <Text>List Screen</Text>
+    <FlatList
+      data={friends}
+      keyExtractor={friend => friend.name}
+      renderItem={({item}) => {
+        return <Text style={styles.text}>{item.name} - Age {item.age}</Text>
+      }}
+    />
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  text: {
+    marginVertical: 50
+  }
+})
 
 export default ListScreen
