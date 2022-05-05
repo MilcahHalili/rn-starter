4329a47a95f04bba0cd1cc7c90ad82dd928a8705
Milcah <code@milcah.dev>
2022-05-05 12:21:53 -0700

    destructures props

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { Button, Text, TouchableOpacity, StyleSheet, View } from 'react-native';
+import { Button, Text, StyleSheet, View } from 'react-native';
 
-const HomeScreen = (props) => {
+const HomeScreen = ({ navigation }) => {
 
   return (
     <View>
       <Text style={styles.text}>Hi, there!</Text>
       <Button
-        onPress={() => props.navigation.navigate('Components')}
+        onPress={() => navigation.navigate('Components')}
         title='Go to Components Demo'
       />
-      <TouchableOpacity onPress={() => props.navigation.navigate('List')}>
-        <Text>Go to List Demo</Text>
-      </TouchableOpacity>
+      <Button
+        onPress={() => navigation.navigate('List')}
+        title='Go to List Demo'
+      />
     </View>
   );
 };
