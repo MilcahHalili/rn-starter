2d206635581bdd6023c69436ad87242ae3e0f449
Milcah <code@milcah.dev>
2022-05-05 21:51:17 -0700

    passes title props to image detail component

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -4,9 +4,15 @@ import ImageDetail from '../components/ImageDetail'
 
 const ImageScreen = () => {
   return <View>
-    <ImageDetail />
-    <ImageDetail />
-    <ImageDetail />
+    <ImageDetail
+      title="Forest"
+    />
+    <ImageDetail
+      title="Beach"
+    />
+    <ImageDetail
+      title="Mountain"
+    />
   </View>
 }
 
