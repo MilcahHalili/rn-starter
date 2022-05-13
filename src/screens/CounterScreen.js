aad7a73efc2c3ceb5c89f6c5f0471a2ab784c513
Milcah <code@milcah.dev>
2022-05-13 14:38:40 -0700

    creates counter screen

---
+++ src/screens/CounterScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const CounterScreen = () => {
+  return <View>
+    <Text>This is the Counter Demo</Text>
+  </View>
+}
+
+const styles = StyleSheet.create({})
+
+export default CounterScreen
